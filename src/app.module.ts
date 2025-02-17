dedba4f74b291a856695cfa6f0a04f01d5633443
codefish <2358514288@qq.com>
2025-02-17 13:49:12 +0800

    // /server

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { ConfigModule } from '@nestjs/config';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root@123',
+      username: process.env.username,
+      password: process.env.password,
       database: 'nestAi',
       synchronize: false, // 数据库同步, 生产环境需要设置为false - 开发环境设置为true
       autoLoadEntities: true, // 自动加载模块注册的实体
