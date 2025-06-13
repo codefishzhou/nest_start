9d83702615dd193d3251e33949306dc9a41f1269
codefish <jiadong.zhou@linkedsmile.com>
2025-06-13 10:21:47 +0800

    websocketservice 数据库连接测试

--- config/configuration.ts
+++ config/configuration.ts
@@ -2,9 +2,9 @@ export default () => ({
   type: 'mysql',
   host: process.env.HOST,
   port: 3306,
-  username: process.env.username,
+  username: process.env.DB_username,
   password: process.env.password,
-  database: 'nestAi',
+  database: 'websocketservice',
   synchronize: false, // 数据库同步, 生产环境需要设置为false - 开发环境设置为true
   autoLoadEntities: true, // 自动加载模块注册的实体
   driver: require('mysql2'),
